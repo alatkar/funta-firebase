b74fddfca5cbc9189bd7b38646fe3361157be738
Atul Alatkar <atalatk@microsoft.com>
2020-08-08 12:56:10 -0700

    CORS middleware

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const fireBaseAuth = require("./util/fireBaseAuth");
+
+const cors = require('cors');
+app.use(cors());
+
 const { db } = require("./util/admin");
 
 //APIs
