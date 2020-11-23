85deb56d665d6a8d7f9559ade73155104c936c12
Atul Alatkar <atalatk@microsoft.com>
2020-11-23 14:22:59 -0800

    users: response and APIs(getUserByEmail, sendPassWordResetEmail)

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -11,6 +11,97 @@ const {
 
 firebase.initializeApp(config);
 
+exports.getUserByEmail = (req, res) => {
+  var auth = firebase.auth();
+  let email = req.params.email;
+  admin
+    .auth()
+    .getUserByEmail(email)
+    .then(function (userRecord) {
+      // See the tables above for the contents of userRecord
+      console.log("Successfully fetched user data:", userRecord.toJSON());
+      return userRecord;
+    })
+    .then((userRecord) => {
+      return db.collection("users").where("userId", "==", userRecord.uid).get();
+    })
+    .then((docs) => {
+      let user;
+      docs.forEach((doc) => (user = doc.data()));
+      return res.json({ response: user });
+    })
+    .catch(function (error) {
+      console.log("Error fetching user data:", error);
+    });
+};
+
+exports.sendPassWordResetEmail = (req, res) => {
+  var auth = firebase.auth();
+  db.collection("users")
+    .where("email", "==", req.params.email)
+    .get()
+    .then((docs) => {
+      let user;
+      docs.forEach((doc) => (user = doc.data()));
+      return user;
+    })
+    .then((doc) => {
+      return auth
+        .sendPasswordResetEmail(doc.email)
+        .then(function () {
+          console.log("Email sent");
+          return res.json({
+            message:
+              "Please check your email for the password reset instructions",
+          });
+          // Email sent.
+        })
+        .catch(function (error) {
+          console.error(err);
+          // An error happened.
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: `${err}` });
+    });
+};
+/*
+exports.sendPassWordResetEmail = (req, res) => {
+  var auth = firebase.auth();
+  db.doc(`/users/${req.params.userName}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return doc;
+      } else {
+        return res
+          .status(404)
+          .json({ message: `User ${req.params.userName} not found` });
+      }
+    })
+    .then((doc) => {
+      return auth
+        .sendPasswordResetEmail(doc.data().email)
+        .then(function () {
+          console.log("Email sent");
+          return res.json({
+            message:
+              "Please check your email for the password reset instructions",
+          });
+          // Email sent.
+        })
+        .catch(function (error) {
+          console.error(err);
+          // An error happened.
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: `${err}` });
+    });
+};
+*/
 exports.loginUser = (req, res) => {
   const user = {
     email: req.body.email,
@@ -19,7 +110,7 @@ exports.loginUser = (req, res) => {
 
   // Data validation
   const { valid, errors } = validateLoginData(user);
-  if (!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json({ message: errors });
 
   firebase
     .auth()
@@ -35,12 +126,10 @@ exports.loginUser = (req, res) => {
       //"auth/wrong-password"
       //"auth/user-not-found"
       //if (err.code === "auth/wrong-password")
-      return res
-        .status(403)
-        .json({
-          error: `${err}`,
-          general: "Wrong credentials, please try again",
-        });
+      return res.status(403).json({
+        error: `${err}`,
+        message: "Wrong credentials, please try again",
+      });
       //else return res.status(500).json({ error: err.code });
     });
 };
@@ -57,24 +146,32 @@ exports.signupUser = (req, res) => {
 
   // Data validation
   const { valid, errors } = validateSignupData(newUser);
-  if (!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json({ message: errors });
 
   let noImage = "no-img.png";
-  
+
   // Sanitize User Type
-  let value = req.body.userType ? req.body.userType.toUpperCase() : enumDefinations.userType.INDIVIDUAL;
-  switch(value)
-  {
+  let value = req.body.userType
+    ? req.body.userType.toUpperCase()
+    : enumDefinations.userType.INDIVIDUAL;
+  switch (value) {
     case enumDefinations.userType.INDIVIDUAL:
     case enumDefinations.userType.ORGANIZATION:
       newUser.userType = value;
       break;
     default:
-      console.log("User Type is incorrect for user ", newUser.userName, " Setting userType as ", enumDefinations.userType.INDIVIDUAL);
-      newUser.userType =   enumDefinations.userType.INDIVIDUAL;
+      console.log(
+        "User Type is incorrect for user ",
+        newUser.userName,
+        " Setting userType as ",
+        enumDefinations.userType.INDIVIDUAL
+      );
+      newUser.userType = enumDefinations.userType.INDIVIDUAL;
   }
   // Sanitize Organization Type: By default set as false;
-  let isNonProfitOrganization = req.body.isNonProfitOrganization ? req.body.isNonProfitOrganization : false;
+  let isNonProfitOrganization = req.body.isNonProfitOrganization
+    ? req.body.isNonProfitOrganization
+    : false;
   newUser.isNonProfitOrganization = isNonProfitOrganization;
 
   let token, userId;
@@ -82,7 +179,9 @@ exports.signupUser = (req, res) => {
     .get()
     .then((doc) => {
       if (doc.exists) {
-        return res.status(400).json({ userName: "This user already exists" });
+        return res
+          .status(400)
+          .json({ message: `The user ${newUser.userName} already exists` });
       } else {
         return firebase
           .auth()
@@ -102,7 +201,7 @@ exports.signupUser = (req, res) => {
         userId: userId,
         userName: newUser.userName,
         userType: newUser.userType,
-        isNonProfitOrganization: newUser.isNonProfitOrganization
+        isNonProfitOrganization: newUser.isNonProfitOrganization,
       };
       db.doc(`/users/${newUser.userName}`).set(userCred);
     })
@@ -114,14 +213,12 @@ exports.signupUser = (req, res) => {
       if (err.code === "auth/email-already-in-use") {
         return res
           .status(400)
-          .json({ email: `Email ${newUser.email} is already in use` });
+          .json({ message: `Email ${newUser.email} is already in use` });
       } else {
-        return res
-          .status(500)
-          .json({
-            error: err.code,
-            general: "Something went wrong, please try again",
-          });
+        return res.status(500).json({
+          error: err.code,
+          message: "Something went wrong, please try again",
+        });
       }
     });
 };
@@ -140,20 +237,19 @@ exports.getUserDetails = (req, res) => {
           .orderBy("createdAt", "desc")
           .get();
       } else {
-        return res.status(404).json({ errror: "User not found" });
+        return res.status(404).json({ message: "User not found" });
       }
     })
     .then((data) => {
       userData.barks = [];
       data.forEach((doc) => {
         let barkCat = "GENERAL";
-        if(doc.data().barkCategory)
-        {
+        if (doc.data().barkCategory) {
           barkCat = doc.data().barkCategory;
         }
         let bark = doc.data();
-        bark.barkId= doc.id;
-        bark.barkCategory= barkCat;
+        bark.barkId = doc.id;
+        bark.barkCategory = barkCat;
         bark.userImageUrl = userData.user.imageUrl;
         userData.barks.push(
           bark
@@ -165,56 +261,67 @@ exports.getUserDetails = (req, res) => {
           likeCount: doc.data().likeCount,
           commentCount: doc.data().commentCount,
           barkId: doc.id,
-        }*/);
+        }*/
+        );
       });
       return;
     })
-    .then(() => {  //Get Pet Profiles  if present
-       console.log("userData..", userData);
-      if(userData.user.petProfiles)
-        return Promise.all( userData.user.petProfiles.map (petProfile => {
-          return db.doc(`/petprofiles/${petProfile.petProfileId}`).get()
-        }))})
+    .then(() => {
+      //Get Pet Profiles  if present
+      console.log("userData..", userData);
+      if (userData.user.petProfiles)
+        return Promise.all(
+          userData.user.petProfiles.map((petProfile) => {
+            return db.doc(`/petprofiles/${petProfile.petProfileId}`).get();
+          })
+        );
+    })
     .then((profiles) => {
       //console.log("Pet profile..", profiles);
       userData.user.petProfiles = [];
-      if(profiles) {
+      if (profiles) {
         profiles.forEach((prof) => {
           let profile = prof.data();
           profile.petProfileId = prof.id;
           userData.user.petProfiles.push(profile);
-        })           
+        });
       }
     })
-    .then(() => { //Get Biz Profiles if present
-      if(userData.user.bizProfiles)
-        return Promise.all( userData.user.bizProfiles.map (bizProfile => {
-          return db.doc(`/bizprofiles/${bizProfile.bizProfileId}`).get()
-    }))})
+    .then(() => {
+      //Get Biz Profiles if present
+      if (userData.user.bizProfiles)
+        return Promise.all(
+          userData.user.bizProfiles.map((bizProfile) => {
+            return db.doc(`/bizprofiles/${bizProfile.bizProfileId}`).get();
+          })
+        );
+    })
     .then((profiles) => {
       //console.log("Biz profile..", profiles);
       userData.user.bizProfiles = [];
-      if(profiles) {        
+      if (profiles) {
         profiles.forEach((prof) => {
           let profile = prof.data();
           profile.bizProfileId = prof.id;
           userData.user.bizProfiles.push(profile);
-        })  
+        });
       }
-      return res.json(userData);
+      return res.json({ response: userData });
     })
     .catch((err) => {
       console.error(err);
-      return res.status(500).json({ error: err.code });
+      res.status(500).json({ message: `${err}` });
     });
 };
 
 // Get user image
 exports.getUserImageUrl = async (userName) => {
   console.log("getUserImageUrl: getting data for :", userName);
-  return db.doc(`/users/${userName}`)
-    .get()
-    /*.then((doc) => {
+  return (
+    db
+      .doc(`/users/${userName}`)
+      .get()
+      /*.then((doc) => {
       if (doc.exists) {        
         console.log("getUserImageUrl: found data ", doc.data().imageUrl);
         return doc.data().imageUrl;
@@ -222,9 +329,10 @@ exports.getUserImageUrl = async (userName) => {
       else
          return null;
     })*/
-    .catch((err) => {
-      console.error("getUserImage Error ", err);
-    });
+      .catch((err) => {
+        console.error("getUserImage Error ", err);
+      })
+  );
 };
 
 // Get own user details
@@ -242,7 +350,7 @@ exports.getAuthenticatedUser = (req, res) => {
       } else {
         return res
           .status(400)
-          .json({ error: `User ${req.user.userName} not found` });
+          .json({ message: `User ${req.user.userName} not found` });
       }
     })
     .then((data) => {
@@ -274,42 +382,50 @@ exports.getAuthenticatedUser = (req, res) => {
       });
       return;
     })
-    .then(() => {  //Get Pet Profiles  if present
-      if(userData.userDetails.petProfiles)
-        return Promise.all( userData.userDetails.petProfiles.map (petProfile => {
-          return db.doc(`/petprofiles/${petProfile.petProfileId}`).get()
-        }))})
+    .then(() => {
+      //Get Pet Profiles  if present
+      if (userData.userDetails.petProfiles)
+        return Promise.all(
+          userData.userDetails.petProfiles.map((petProfile) => {
+            return db.doc(`/petprofiles/${petProfile.petProfileId}`).get();
+          })
+        );
+    })
     .then((profiles) => {
       //console.log("Pet profile..", profiles);
       userData.userDetails.petProfiles = [];
-      if(profiles) {
+      if (profiles) {
         profiles.forEach((prof) => {
           let profile = prof.data();
           profile.petProfileId = prof.id;
           userData.userDetails.petProfiles.push(profile);
-        })           
+        });
       }
     })
-    .then(() => { //Get Biz Profiles if present
-      if(userData.userDetails.bizProfiles)
-        return Promise.all( userData.userDetails.bizProfiles.map (bizProfile => {
-          return db.doc(`/bizprofiles/${bizProfile.bizProfileId}`).get()
-      }))})
+    .then(() => {
+      //Get Biz Profiles if present
+      if (userData.userDetails.bizProfiles)
+        return Promise.all(
+          userData.userDetails.bizProfiles.map((bizProfile) => {
+            return db.doc(`/bizprofiles/${bizProfile.bizProfileId}`).get();
+          })
+        );
+    })
     .then((profiles) => {
       //console.log("Biz profile..", profiles);
       userData.userDetails.bizProfiles = [];
-      if(profiles) {
+      if (profiles) {
         profiles.forEach((prof) => {
           let profile = prof.data();
           profile.bizProfileId = prof.id;
           userData.userDetails.bizProfiles.push(profile);
-        })  
+        });
       }
-      return res.json(userData);
+      return res.json({ response: userData });
     })
     .catch((err) => {
       console.error(err);
-      return res.status(500).json({ error: err.code });
+      res.status(500).json({ message: `${err}` });
     });
 };
 
@@ -325,7 +441,52 @@ exports.addUserDetails = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      return res.status(500).json({ error: err.code });
+      res.status(500).json({ message: `${err}` });
+    });
+};
+
+exports.patchUser = (req, res) => {
+  //console.log(`Patching PetProfile Body: ${Object.keys(req.body)}`);
+  const document = db.doc(`/users/${req.user.userName}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      if (doc.data().userName !== req.user.userName) {
+        return res.status(403).json({
+          message: "Unauthorized. Can not update other user's PetProfile",
+        });
+      } else {
+        return doc;
+      }
+    })
+    .then((doc) => {
+      //Update user
+      console.log("Doc data: ", doc.data());
+      // TODO: Validating if fields exist. We might need to allow to enter fields
+      /*
+      for (const [key, value] of Object.entries(req.body)) {
+        if (key === "imageUrl") {
+          //Allow imageUrl to be inserted if not present
+          continue;
+        }
+        if (!doc.data().hasOwnProperty(key)) {
+          return res
+            .status(404)
+            .json({ message: `PetProfile property ${key} not found` });
+        }
+        //console.log(`${key}: ${value}`);        
+      }*/
+      return doc.ref.update(req.body);
+    })
+    .then(() => {
+      res.json({ message: `User ${req.user.userName} updated successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: `${err}` });
     });
 };
 
@@ -346,7 +507,7 @@ exports.uploadUserImage = (req, res) => {
   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
     console.log(fieldname, file, filename, encoding, mimetype);
     if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
-      return res.status(400).json({ error: "Wrong file type submitted" });
+      return res.status(400).json({ message: "Wrong file type submitted" });
     }
     // my.image.png => ['my', 'image', 'png']
     const imageExtension = filename.split(".")[filename.split(".").length - 1];
@@ -384,7 +545,7 @@ exports.uploadUserImage = (req, res) => {
       })
       .catch((err) => {
         console.error(err);
-        return res.status(500).json({ error: "something went wrong" });
+        res.status(500).json({ message: `${err}` });
       });
   });
   busboy.end(req.rawBody);
@@ -405,6 +566,6 @@ exports.markNotificationsRead = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      return res.status(500).json({ error: err.code });
+      res.status(500).json({ message: `${err}` });
     });
 };
