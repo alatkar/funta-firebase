b4e7e9633f49f28937982823ab7bb628710b16cc
Atul Alatkar <atalatk@microsoft.com>
2020-12-09 07:15:39 -0800

    Basic search for user names

---
+++ functions/handlers/search.js
@@ -0,0 +1,24 @@
+const { admin, db } = require("../util/admin");
+const config = require("../util/config");
+const enumDefinations = require("../util/enums");
+
+exports.postSearchByUserName = (req, res) => {
+  let users = [];
+  db.collection("users")
+    .orderBy("userName")
+    .startAt(req.params.userName)
+    .get()
+    .then((docs) => {
+      docs.forEach((doc) => users.push(doc.data().userName));
+      return users;
+    })
+    .then(() => {
+      return res.json({
+        response: {users},
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: `${err}` });
+    });
+};
